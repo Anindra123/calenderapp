5c408bb057894f3df0ad446739620e74664cede0
Anindra123 <bivasdas911@gmail.com>
2024-01-11 11:49:06 +0600

    fix typescript errors

--- src/component/DateBox/DateBox.tsx
+++ src/component/DateBox/DateBox.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import { useRef } from "react";
 import { months } from "../../constants/Constant";
 import { DateInfo } from "../../types/DateTypes"
 import "./DateBox.css"
